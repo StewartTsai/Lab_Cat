666a799c0772f9e93efc4a9c5aad44b166237dd8
StewartTsai <47299398+StewartTsai@users.noreply.github.com>
2019-10-29 14:21:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-    a=1
+   
     
     
 
